2dfb42b49eeb673139266a7ba56ac90af6c0ef17
Ryan Schultz <schultz.ryan.w@gmail.com>
2017-05-03 12:49:45 -0700

    try

--- src/assets/main.js
+++ src/assets/main.js
@@ -15,7 +15,7 @@ function guess() {
             showReplay();
         }
         else if(attempt >= 10){
-            setMessage("You Lose! :(");
+            setMessage("You Lose!");
             showAnswer(false);
             showReplay();
         }
